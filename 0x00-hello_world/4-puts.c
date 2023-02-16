177c99f34ab6bf4e571a73c507759194c5d9c879
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-02-16 17:50:57 +0000

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main - A programme that displays a string of texts using puts function.
+ * Return: 0 (success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
