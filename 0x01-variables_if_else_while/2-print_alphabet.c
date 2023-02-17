3a60e3147b502c910cfd78c08b7c7bd634fdf165
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-02-17 23:28:02 +0000

    printing the alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - displaying the english alphabets in lower case
+ * Return: always (Success)
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	putchar(lower);
+	for (lower = 'a'; lower <= 'z'; lower++)
+	putchar(lower);
+	putchar('\n');
+	return (0);
+}
