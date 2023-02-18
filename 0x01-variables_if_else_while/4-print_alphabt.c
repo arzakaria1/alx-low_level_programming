4dee331653aa474358e81705ab4b18c7c8ba7776
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-02-18 00:25:49 +0000

    printing the alphabets and ommiting e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - a program that displays the alphabets and omiting some
+ * Return: always (success)
+ */
+int main(void)
+{
+	char lower, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+	if (lower != e && lower != q)
+	putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
