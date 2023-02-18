546c5e7417c08681d9d4b98aff7f8e8dc5433284
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-02-18 02:14:34 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - a program that prints  possible different combinations of two digits.
+ * Return: always (success)
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = '0'; m < '9'; m++)
+	{
+	for (n = m + 1; n <= '9'; n++)
+	{
+	if (n != 0)
+	{
+	putchar(m);
+	putchar(n);
+	if (m == '0' && m == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
