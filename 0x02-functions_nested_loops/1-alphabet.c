72134696bd5d8c214ab33d4c5ecce05e2cab719c
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-02-21 14:35:01 +0000

    display lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - display the alphabet in lower case
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
