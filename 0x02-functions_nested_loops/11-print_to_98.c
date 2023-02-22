090d2a09571a951752f23e6dcedb0979d53018bc
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-02-22 00:50:40 +0000

    hours

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * @n: the number to start from
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+
+	
