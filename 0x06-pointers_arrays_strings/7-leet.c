a38648aab02b3325cbc3d1df5d3d03f1a588e480
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-03-03 01:32:16 +0000

    7-encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * leet - encode into 1337speak
- * @n: input value
- * Return: n value
+ * leet - a function that encodes a string into 1337.
+ * @n: the number to be encoded
+ * Return: value of n
  */
 char *leet(char *n)
 {
@@ -12,13 +12,13 @@ char *leet(char *n)
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
-		}
+	for (j = 0; j < 10; j++)
+	{
+	if (n[i] == s1[j])
+	{
+	n[i] = s2[j];
+	}
+	}
 	}
 	return (n);
 }
