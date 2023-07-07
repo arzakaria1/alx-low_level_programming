b461ce17da355f1b5413808032a15a32aca3d43a
Zakaria Abdul-Rasheed <zakariaabdulrasheed2@gmail.com>
2023-07-07 02:07:47 +0000

    Congratulations!

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,7 +5,7 @@
  * @n: pointer to the number to change
  * @index: index of the bit to clear
  *
- * Return: 1 for success, -1 for failure
+ * Return: 1 for success, and -1 for failure
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
